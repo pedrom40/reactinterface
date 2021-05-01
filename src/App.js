cdd8ab53463d2cf36e8d25deb2d9d62b00e8bfc1
pedrom40 <pedro.morin1980@gmail.com>
2021-05-01 08:47:17 -0500

    configured tailwindcss and craco

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BiArchive } from 'react-icons/bi';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive />Your Appointments
+      </h1>
     </div>
   );
 }
