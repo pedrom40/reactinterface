9b477447c00d36d2ca37dace25e2b6eff6d31f15
pedrom40 <pedro.morin1980@gmail.com>
2021-05-02 11:00:43 -0500

    enabled sort and order by functions

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import AppointmentInfo from './components/AppointmentInfo';
 function App() {
   let [appointmentList, setAppointmentList] = useState([]);
   let [query, setQuery] = useState('');
+  let [sortBy, setSortBy] = useState('petName');
+  let [orderBy, setOrderBy] = useState('asc');
 
   const filteredAppointments = appointmentList.filter(
     item => {
@@ -17,7 +19,13 @@ function App() {
         item.aptNotes.toLowerCase().includes(query.toLowerCase())
       )
     }
-  )
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order : 1 * order
+    )
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -37,7 +45,13 @@ function App() {
         <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments
       </h1>
       <AddAppointment />
-      <Search query={query} onQueryChange={myQuery => setQuery(myQuery)} />
+      <Search 
+        query={query} 
+        onQueryChange={myQuery => setQuery(myQuery)} 
+        orderBy={orderBy}
+        onOrderByChange={mySort => setOrderBy(mySort)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)} />
 
       <ul className="divide-y divide-gray-200">
         {filteredAppointments
